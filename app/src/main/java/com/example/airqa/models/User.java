2d6f17ef9cca9046a0a1bdb39a26c5d68122ea4d
thu4n <thuan.tongg@gmail.com>
2023-11-03 21:59:17 +0700

    create User class

---
+++ app/src/main/java/com/example/airqa/models/User.java
@@ -0,0 +1,45 @@
+package com.example.airqa.models;
+
+public class User {
+    private String client_id;
+    private String email;
+    private String username;
+    private String passwrod;
+    private final String grant_type = "password";
+
+    public String getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(String client_id) {
+        this.client_id = client_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswrod() {
+        return passwrod;
+    }
+
+    public void setPasswrod(String passwrod) {
+        this.passwrod = passwrod;
+    }
+
+    public String getGrant_type() {
+        return grant_type;
+    }
+}
