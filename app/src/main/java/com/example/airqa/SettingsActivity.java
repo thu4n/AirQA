1625efa2df2c0d61bdef929ff1ade71b2f15f1d3
dangto2003 <nguyendangto2502@gmail.com>
2023-11-07 14:52:11 +0700

    bottom nav and setting activity

---
+++ app/src/main/java/com/example/airqa/SettingsActivity.java
@@ -0,0 +1,61 @@
+package com.example.airqa;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.preference.PreferenceFragment;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceScreen;
+
+public class SettingsActivity extends BaseActivity {
+    LinearLayout dynamicContent,bottomNavBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_matching);
+
+
+        //dynamically include the  current activity layout into baseActivity layout.now all the view of baseactivity is   accessible in current activity.
+        dynamicContent = (LinearLayout)  findViewById(R.id.dynamicContent);
+        bottomNavBar= (LinearLayout) findViewById(R.id.bottomNavBar);
+        View wizard = getLayoutInflater().inflate(R.layout.activity_settings, null);
+        dynamicContent.addView(wizard);
+
+        //get the reference of RadioGroup.
+
+        RadioGroup rg=(RadioGroup)findViewById(R.id.radioGroup1);
+        RadioButton rb=(RadioButton)findViewById(R.id.settings);
+
+        // Change the corresponding icon and text color on nav button click.
+
+        rb.setTextColor(Color.parseColor("#1E9CE1"));
+        rb.setCompoundDrawableTintList(ColorStateList.valueOf(getColor(R.color.primary)));
+
+
+        if (findViewById(R.id.idFrameLayout) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+            // below line is to inflate our fragment.
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.idFrameLayout, new SettingsFragment())
+                    .setReorderingAllowed(true)
+                    .commit();
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/airqa/SettingsFragment.java
@@ -0,0 +1,16 @@
+package com.example.airqa;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import androidx.annotation.Nullable;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        // Load the preferences from an XML resource
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+}
